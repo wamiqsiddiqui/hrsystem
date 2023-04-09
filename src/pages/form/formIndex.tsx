ed396da119351b40802af136d8788ccb37590e6d
wamiqsiddiqui <wamiqsiddiqui30@gmail.com>
2023-04-09 23:36:46 +0500

    creating form

---
+++ src/pages/form/formIndex.tsx
@@ -0,0 +1,10 @@
+import { Box, Button, TextField } from "@mui/material";
+import { Formik } from "formik";
+import * as yup from "yup";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { Header } from "../../components/Header";
+
+const Form = () => {
+  return <Box m={"20px"}></Box>;
+};
+export default Form;
